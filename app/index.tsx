2896e09fd6025b157451e6b8e2daec92c57383b2
RWOverdijk <r.w.overdijk@gmail.com>
2024-09-05 11:01:24 +0200

    chore(project): initial commit

---
+++ app/index.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import { Button } from '~/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/ui/dialog';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select';
+import { Text } from '~/components/ui/text';
+
+export default function Example() {
+  const insets = useSafeAreaInsets();
+  const contentInsets = {
+    top: insets.top,
+    bottom: insets.bottom,
+    left: 12,
+    right: 12,
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant='outline'>
+          <Text>Edit Profile</Text>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className='sm:max-w-[425px]'>
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <Select defaultValue={{ value: 'apple', label: 'Apple' }}>
+          <SelectTrigger className='w-[250px]'>
+            <SelectValue
+              className='text-foreground text-sm native:text-lg'
+              placeholder='Select a fruit'
+            />
+          </SelectTrigger>
+          <SelectContent insets={contentInsets} className='w-[250px]'>
+            <SelectGroup>
+              <SelectLabel>Fruits</SelectLabel>
+              <SelectItem label='Apple' value='apple'>
+                Apple
+              </SelectItem>
+              <SelectItem label='Banana' value='banana'>
+                Banana
+              </SelectItem>
+              <SelectItem label='Blueberry' value='blueberry'>
+                Blueberry
+              </SelectItem>
+              <SelectItem label='Grapes' value='grapes'>
+                Grapes
+              </SelectItem>
+              <SelectItem label='Pineapple' value='pineapple'>
+                Pineapple
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button>
+              <Text>OK</Text>
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
